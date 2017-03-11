9341db9d589bda12dc6c8501f9362fdcdba6134b
Angela <yunshi@upenn.edu>
2017-03-10 20:25:10 -0500

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
 var coffeeSources = [
@@ -19,5 +20,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
   gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
 });
