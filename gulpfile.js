7a252dc0e645e744782fb0e3ba8547c8830907e1
Angela <yunshi@upenn.edu>
2017-03-10 21:56:03 -0500

    added minified html

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 var env,
     coffeeSources,
@@ -18,11 +19,13 @@ var env,
 env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
-  outputDir = 'builds/developmenet/';
+  outputDir = 'builds/development/';
   sassStyle = 'expanded';
 } else {
   outputDir = 'builds/production/';
   sassStyle = 'compressed';
+  console.log('production mode');
+  console.log(env === 'production');
 }
 
 
@@ -71,7 +74,7 @@ gulp.task('watch', function(){
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(outputDir + 'js/*.json', ['json']);
 });
 
@@ -83,10 +86,9 @@ gulp.task('connect', function(){
 });
 
 gulp.task('html', function(){
-  gulp.src(htmlSources)
-    .pipe(connect.reload());
-});gulp.task('html', function(){
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload());
 });
 
