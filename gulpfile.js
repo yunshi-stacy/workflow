61b986177e0a92412a6cf4a21e53be5a1386cd48
Angela <yunshi@upenn.edu>
2017-03-10 20:21:16 -0500

    concat js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
-var coffeeSources = ['components/coffee/*.coffee'];
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
+
+var coffeeSources = [
+  'components/coffee/*.coffee'
+];
+var jsSources = [
+  'components/scripts/*.js'
+];
 
 gulp.task('coffee', function(){
   gulp.src(coffeeSources)
     .pipe(coffee({bare: true})
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'));
+});
+gulp.task('js', function(){
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'));
 });
